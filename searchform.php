0360f17032cf10ceef3470647b029095c62f7900
Garry Ing <garry.ing@gmail.com>
2011-03-13 00:51:39 -0500

    search form template. might not need in future

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div id="search">
+        <input type="text" value="Looking For Something?" name="s" id="s" />
+    </div>
+</form>
