61b0ab7187b213a1a8a297223274a1a5f439d327
Garry Ing <garry.ing@gmail.com>
2011-03-13 20:31:12 -0400

    Added first last check to keyboard shortcuts. removed show hide

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,20 +1,16 @@
-// Some way of hiding and revealing a navigation. Just an idea.
-$("#footer-header").click(function () {
-    $("#footer-menu").slideToggle("slow");
-  });
-
 // Probably going to use Masonry for some grid action.
 $('.illu-work').masonry({ columnWidth: 310, animate: true, itemSelector: '.gallery-item' });
 
 $('#year-archive').masonry({ columnWidth: 160, animate: true });
 
-
 // A quick go at keyboar nav. I'm sure there is a better way at this.
 $(document.documentElement).keyup(function (event) {
   // handle cursor keys
-  if (event.keyCode == 37) {
+  if (event.keyCode == 37 && ($(".prev-link a").length)) {
 		window.location = $('.prev-link a').attr('href');
-  } else if (event.keyCode == 39) {
+  } else if (event.keyCode == 39 && ($(".next-link a").length)) {
 		window.location = $('.next-link a').attr('href');  
   }
 });
+
+
