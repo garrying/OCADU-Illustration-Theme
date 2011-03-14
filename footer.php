0e3ab6f55c4b4d7561e7e00e53c0fcc5a4e1c1df
Garry Ing <garry.ing@gmail.com>
2011-03-13 20:31:49 -0400

    start of a fancy footer

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<div id="footer">
+	<div class="container">
+		<blockquote>Had I the heavens’ embroidered cloths, Enwrought with golden and silver light, The blue and the dim and the dark cloths<br />
+		Of night and light and the half light,
+		I would spread the cloths under your feet:
+		But I, being poor, have only my dreams;<br />
+		I have spread my dreams under your feet;
+		Tread softly because you tread on my dreams.</blockquote>
+		<p>Copyright © 2010</p>
+		<p>Respective Authors</p>
+	</div>
+</div>
+
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.masonry.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/global.js"></script>
