4c75b440a0c68812f9d36c31edc3df8605f7b1ab
Garry Ing <garry.ing@gmail.com>
2011-03-16 12:14:39 -0400

    start of a fancy 404

--- 404.php
+++ 404.php
@@ -2,11 +2,14 @@
 
 <div id="container">
 
+<div id="page-not-found">
 	<h1 class="entry-title"><?php _e( 'Not Found'); ?></h1>
 	<div class="entry-content">
 		<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.'); ?></p>
 		<?php get_search_form(); ?>
+		<p class="small">Press enter to submit</p>
 	</div><!-- .entry-content -->
+</div>
 
 </div> <!-- #container -->
 
