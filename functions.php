dcd3d772ad758a8e963f76ef0befdeb9f02b1926
Garry Ing <garry.ing@gmail.com>
2011-04-25 21:46:35 -0400

    Added smooth loading for index and trimming of default wp header

--- functions.php
+++ functions.php
@@ -26,7 +26,6 @@ register_nav_menus( array(
 	'primary' => __( 'Primary Navigation', 'ocaduillustration' ),
 ) );
 
-
 // Limit search to Events and Illustrators
 
 function SearchFilter($query) {
@@ -37,4 +36,10 @@ function SearchFilter($query) {
 }
  
 add_filter('pre_get_posts','SearchFilter');
+
+// Trimming default wp head
+
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+
 ?>
