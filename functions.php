25eb18f436357ee66533e58e74b1c12016d61c8b
Garry Ing <garry.ing@gmail.com>
2011-05-26 22:26:19 -0400

    Added menu states for Event archive

--- functions.php
+++ functions.php
@@ -26,14 +26,6 @@ register_nav_menus( array(
 	'primary' => __( 'Primary Navigation', 'ocaduillustration' ),
 ) );
 
-// Limit search to Illustrators
-
-function SearchFilter($query) {
-    if ($query->is_search) {
-        $query->set('post_type', array( 'illustrator' ));
-    }
-    return $query;
-}
  
 add_filter('pre_get_posts','SearchFilter');
 
@@ -47,5 +39,19 @@ function my_remove_recent_comments_style() {
 	global $wp_widget_factory;
 	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'  ) );
 }
+
+// Added menu functionality for Events post type in menu
+
+add_filter( 'nav_menu_css_class', 'x_nav_menu_css_class', 10, 3 );
+function x_nav_menu_css_class( $classes, $item = null, $args = null ) {
+	$post_type = "event";
+       if ( is_singular() || is_post_type_archive( $post_type ) ) {
+               $pto = get_post_type_object( get_query_var('post_type') );
+               if ( $pto->rewrite['slug'] == $item->post_name )
+                       $classes[] = 'current-menu-item';
+       }
+       return $classes;
+			
+	   }
 
 ?>
