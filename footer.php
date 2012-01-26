aa911da7b901240f08c3809ca6a13c8da3432119
Garry Ing <garry.ing@gmail.com>
2012-01-26 17:30:09 -0500

    added additional footer text

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 	</div><!-- #main -->
 
-	<footer id="colophon" role="contentinfo">		
+	<footer id="colophon" role="contentinfo" class="container">		
 		<div id="copyright">
 			<p>Copyright © 2012 Respective Authors</p>
+			<p>Maintained by the Illustration Department at <a href="http://www.ocadu.ca">OCAD University</a>  ♥</p>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
