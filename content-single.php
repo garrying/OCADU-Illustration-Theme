5d4df99875163e728f2091328d61a927b7dda064
Garry Ing <garry.ing@gmail.com>
2012-01-26 17:34:42 -0500

    added post navigation and spans for shadow gradient effect

--- content-single.php
+++ content-single.php
@@ -1,7 +1,4 @@
 <article <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		
@@ -9,25 +6,45 @@
 			    $gallery_shortcode = '[gallery size="medium" columns="0"]';
 					print apply_filters( 'the_content', $gallery_shortcode );
 		?>
+		<aside>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<hr />
+		</header><!-- .entry-header -->
 			
 				<?php if ( get_post_meta($post->ID, 'illu_sites', true) ) : ?>
-					<div class="info site"><p><a title="Visit illustrator's website" href="<?php echo get_post_meta($post->ID, 'illu_sites', true) ?>"><?php echo get_post_meta($post->ID, 'illu_sites', true) ?></a></p></div>
+					<div class="info site"><a title="Visit illustrator's website" href="<?php echo get_post_meta($post->ID, 'illu_sites', true) ?>"><?php echo get_post_meta($post->ID, 'illu_sites', true) ?></a></div><hr />
+					
 				<?php endif; ?>
 
 				<?php if ( get_post_meta($post->ID, 'illu_sites_2', true) ) : ?>
-					<div class="info site"><p><a title="Visit illustrator's website" href="<?php echo get_post_meta($post->ID, 'illu_sites_2', true) ?>"><?php echo get_post_meta($post->ID, 'illu_sites_2', true) ?></a></p></div>
+					<div class="info site"><a title="Visit illustrator's website" href="<?php echo get_post_meta($post->ID, 'illu_sites_2', true) ?>"><?php echo get_post_meta($post->ID, 'illu_sites_2', true) ?></a></div><hr />
 				<?php endif; ?>
 
 				<?php if ( get_post_meta($post->ID, 'illu_email', true) ) : ?>
-				 <div class="info email"><p><a title="Email <?php the_title(); ?>" href="mailto:<?php echo get_post_meta($post->ID, 'illu_email', true) ?>"><?php echo get_post_meta($post->ID, 'illu_email', true) ?></a></p></div>
+				 <div class="info email"><a title="Email <?php the_title(); ?>" href="mailto:<?php echo get_post_meta($post->ID, 'illu_email', true) ?>"><?php echo get_post_meta($post->ID, 'illu_email', true) ?></a></div><hr />
 				<?php endif; ?>
 
 				<?php if ( get_post_meta($post->ID, 'illu_phone', true) ) : ?>
-				 <div class="info phone"><p><?php echo get_post_meta($post->ID, 'illu_phone', true) ?></p></div>
+				 <div class="info phone"><?php echo get_post_meta($post->ID, 'illu_phone', true) ?></div>	<hr />
 				<?php endif; ?>
 			
 		
 		<?php the_content(); ?>
+		
+		<nav id="nav-single">
+			<ul>
+			<h3 class="assistive-text"><?php _e( 'Post navigation' ); ?></h3>
+			<li class="nav-previous"><?php previous_post_link_plus( array('order_by' => 'post_title', 'format' => '%link', 'in_same_tax' => true) ); ?></li>
+			<li class="nav-next"><?php next_post_link_plus( array('order_by' => 'post_title', 'format' => '%link', 'in_same_tax' => true) ); ?></li>
+			</ul>
+		</nav><!-- #nav-single -->
+		
+		<span class="vertical-rule-main"></span>
+		<span class="vertical-rule-corner-top"></span>
+		<span class="vertical-rule-corner-bottom"></span>
+		
+		</aside><!-- aside -->
 		
 	</div><!-- .entry-content -->
 
