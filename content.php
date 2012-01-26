9714ca12aad363b1ec0a3734407c662ee21d5e97
Garry Ing <garry.ing@gmail.com>
2012-01-26 17:36:25 -0500

    cleaned up conditionals and markup for pages

--- content.php
+++ content.php
@@ -1,13 +1,18 @@
 	<article <?php post_class(); ?>>
 		<header class="entry-header">
 
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-			
-			<?php 
-			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-			  the_post_thumbnail('thumbnail');
-			} 
-			?>
+		<?php if ( is_page() ) : // Only display Excerpts for Search ?>
+
+			<h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
+
+		<?php else : ?>
+
+			<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+			   <h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
+			   <?php the_post_thumbnail('thumbnail', array('alt' => 'Thumbnail of '.get_the_title().'', 'title' => ''.get_the_title().'' )); ?>
+			</a>
+
+		<?php endif; ?>
 
 		</header><!-- .entry-header -->
 
@@ -15,35 +20,11 @@
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
 		</div><!-- .entry-summary -->
-		<?php else : ?>
+		<?php elseif ( is_page() ) : ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:' ) . '</span>', 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
 		<?php endif; ?>
 
-		<footer class="entry-meta">
-			<?php $show_sep = false; ?>
-			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ' ) );
-				if ( $categories_list ):
-			?>
-			<span class="cat-links">
-				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
-				$show_sep = true; ?>
-			</span>
-			<?php endif; // End if categories ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ' ) );
-				if ( $tags_list ):
-				if ( $show_sep ) : ?>
-			<span class="sep"> | </span>
-				<?php endif; // End if $show_sep ?>
-			<?php endif; // End if $tags_list ?>
-			<?php endif; // End if 'post' == get_post_type() ?>
 
-		</footer><!-- #entry-meta -->
 	</article><!-- #post-<?php the_ID(); ?> -->
