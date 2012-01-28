3f9438522a6cc5d2930555f7a257cf649163d37a
Garry Ing <garry.ing@gmail.com>
2012-01-27 22:53:46 -0500

    registered masonry.js

--- functions.php
+++ functions.php
@@ -16,6 +16,8 @@ if (!function_exists('load_my_scripts')) {
 			wp_deregister_script( 'jquery' );
 			wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js', '','',true);
 			wp_enqueue_script('jquery');
+			wp_register_script('masonry', get_template_directory_uri().'/assets/js/jquery.masonry.min.js', array('jquery'), '1.0', true );
+			wp_enqueue_script('masonry');
 			wp_register_script('myscript', get_template_directory_uri().'/assets/js/ui.js', array('jquery'), '1.0', true );
 			wp_enqueue_script('myscript');
 		}
