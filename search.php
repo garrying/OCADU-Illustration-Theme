fa830c9be205037479c0ff2f0027f704985f28c8
Garry Ing <garry.ing@gmail.com>
2012-01-30 23:06:00 -0500

    adding search results template

---
+++ search.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+<?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); ?>
+
+<h1 id="year-title">
+<?php if ($term->name == null)
+		wp_title("");
+	else
+		echo $term->name;
+ ?>
+</h1>
+
+		<section id="primary">
+
+			
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+				<?php query_posts($query_string . '&orderby=title&order=ASC');?><!-- Query by Title -->
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+
+				<?php ocadillu_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<article class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found' ); ?></h1>
+						<h2><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.' ); ?></h2>
+					</header><!-- .entry-header -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</section><!-- #primary -->
+
+<?php get_footer(); ?>
