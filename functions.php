b029117b5688e543e382c164cfcb5f79858905d9
Garry Ing <garry.ing@gmail.com>
2012-02-01 21:04:44 -0500

    added a few snippets from bones to cleanup header meta

--- functions.php
+++ functions.php
@@ -7,6 +7,24 @@
 add_theme_support( 'post-thumbnails' );
 
 /**
+ * Wordpress Default Header Cleanup
+ */
+
+function ocad_head_cleanup() {
+	remove_action( 'wp_head', 'feed_links_extra', 3 );                    // Category Feeds
+	remove_action( 'wp_head', 'feed_links', 2 );                          // Post and Comment Feeds
+	remove_action( 'wp_head', 'rsd_link' );                               // EditURI link
+	remove_action( 'wp_head', 'wlwmanifest_link' );                       // Windows Live Writer
+	remove_action( 'wp_head', 'index_rel_link' );                         // index link
+	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );            // previous link
+	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );             // start link
+	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Links for Adjacent Posts
+	remove_action( 'wp_head', 'wp_generator' );                           // WP version
+}
+
+add_action('init', 'ocad_head_cleanup');
+
+/**
  * Load some scripts please.
  */
  
