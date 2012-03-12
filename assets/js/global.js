7a7e125d28ac9f088d373b4d619079285d63f03a
Garry Ing <garry.ing@gmail.com>
2012-03-11 21:13:27 -0400

    final 2011 commit to match live server

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,57 +1,55 @@
-// Homepage Imageload Biznezz
-$(function () {
-	$('.home img').hide();//hide all the images on the page	
-});
-
-var i = 0;//initialize
-var int=0;//Internet Explorer Fix
-$(window).bind("load", function() {//The load event will only fire if the entire page or document is fully loaded
-	var int = setInterval("doThis(i)",100);//500 is the fade in speed in milliseconds
-	$('.home .post').show();//hide all the images on the page	
+$(document).ready(function() {
+ 	// Homepage Imageload Biznezz
+	$(function () {
+		$('.home img').hide();//hide all the images on the page	
+	});
 	
-});
-
-function doThis() {
-	var images = $('.home img').length;//count the number of images on the page
-	if (i >= images) {// Loop the images
-		clearInterval(int);//When it reaches the last image the loop ends
+	var i = 0;//initialize
+	var int=0;//Internet Explorer Fix
+	
+	function doCascade(delay) {
+		$('.home .post').each(function (i) {
+			$(this).delay(i * delay).fadeIn('fast').find('img').delay(i * delay).fadeIn();
+		});
 	}
-	$('img:hidden').eq(0).fadeIn(100);//fades in the hidden images one by one
-	i++;//add 1 to the count
-}
+	
+	$(window).bind("load", function() {//The load event will only fire if the entire page or document is fully loaded
+		doCascade(200);
+	});
 
-// Masonry for some grid action
-$('.grid-large').masonry({ columnWidth: 310, animate: true, itemSelector: '.post' });
+	// Masonry for some grid action
+	$('.grid-large').masonry({ columnWidth: 310, animate: true, itemSelector: '.post' });
 
-$('.grid-large').masonry({ columnWidth: 310, animate: true, itemSelector: '.gallery-item' });
+	$('.grid-large').masonry({ columnWidth: 310, animate: true, itemSelector: '.gallery-item' });
 
-$('.grid-small').masonry({ columnWidth: 155, animate: true, itemSelector: '.post' });
+	$('.grid-small').masonry({ columnWidth: 155, animate: true, itemSelector: '.post' });
 
-$('#illustrator-archive').masonry({ columnWidth: 155, animate: true, itemSelector: '.post' });
+	$('#illustrator-archive').masonry({ columnWidth: 155, animate: true, itemSelector: '.post' });
 
-// A quick go at keyboar nav. I'm sure there is a better way at this
-$(document.documentElement).keyup(function (event) {
-  // handle cursor keys, illustrator navigation
-  if (event.keyCode == 37 && ($(".prev-link a").length)) {
-		window.location = $('.prev-link a').attr('href');
-  } else if (event.keyCode == 39 && ($(".next-link a").length)) {
-		window.location = $('.next-link a').attr('href');  
-  }
+	// A quick go at keyboar nav. I'm sure there is a better way at this
+	$(document.documentElement).keyup(function (event) {
+	  // handle cursor keys, illustrator navigation
+	  if (event.keyCode == 37 && ($(".prev-link a").length)) {
+			window.location = $('.prev-link a').attr('href');
+	  } else if (event.keyCode == 39 && ($(".next-link a").length)) {
+			window.location = $('.next-link a').attr('href');  
+	  }
 
-  // Work navigation
-  if (event.keyCode == 37 && ($("#image-navigation .nav-previous a").length)) {
-		window.location = $('#image-navigation .nav-previous a').attr('href');
-  } else if (event.keyCode == 39 && ($("#image-navigation .nav-next a").length)) {
-		window.location = $('#image-navigation .nav-next a').attr('href');  
-  }
+	  // Work navigation
+	  if (event.keyCode == 37 && ($("#image-navigation .nav-previous a").length)) {
+			window.location = $('#image-navigation .nav-previous a').attr('href');
+	  } else if (event.keyCode == 39 && ($("#image-navigation .nav-next a").length)) {
+			window.location = $('#image-navigation .nav-next a').attr('href');  
+	  }
 
+	});
+
+	// Jump menu business
+	function MM_jumpMenu(targ,selObj,restore){ //v3.0
+		  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+		  if (restore) selObj.selectedIndex=0;
+		}
+
+	// Jump menu nice-ness 
+	$("select").uniform();
 });
-
-// Jump menu business
-function MM_jumpMenu(targ,selObj,restore){ //v3.0
-	  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
-	  if (restore) selObj.selectedIndex=0;
-	}
-
-// Jump menu nice-ness 
-$("select").uniform();
--- footer.php
+++ footer.php
@@ -47,11 +47,12 @@
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.uniform.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/global.js"></script>
 
-<?php
-if ( function_exists( 'yoast_analytics' ) ) { 
-  yoast_analytics(); 
-}
-?>
+<script>
+    var _gaq=[['_setAccount','UA-16173154-1'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
 
 <?php
 	/* Always have wp_footer() just before the closing </body>
