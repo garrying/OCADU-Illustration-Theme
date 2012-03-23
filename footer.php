359eb25a9ecff99aea9c9090b3d77bbf59add01b
Garry Ing <garry.ing@gmail.com>
2012-03-23 13:08:57 -0400

    footer formatting and refinements of search and single navigation area

--- footer.php
+++ footer.php
@@ -4,8 +4,7 @@
 		<div id="copyright">
 			<div id="toggle"></div>
 			<p>Copyright © 2012 Respective Authors</p>
-			<p>Maintained by the Illustration Department at <a href="http://www.ocadu.ca" title="Link to OCAD University's website">OCAD University</a></p>
-			
+			<p>Maintained by the Illustration Department at <a href="http://www.ocadu.ca" title="Link to OCAD University's website">OCAD University</a></p>		
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
