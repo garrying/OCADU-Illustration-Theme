a46007ec2d81c32569a92de42e677d176c193b60
Garry Ing <garry.ing@gmail.com>
2012-04-28 10:02:09 -0400

    analytics stuff

--- footer.php
+++ footer.php
@@ -19,5 +19,12 @@
 
 <?php wp_footer(); ?>
 
+<script>
+    var _gaq=[['_setAccount','UA-16173154-1'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+
 </body>
 </html>
