48c551908344ee03f43c416b9090fb12bb0c8a04
Garry Ing <garry.ing@gmail.com>
2013-02-03 22:27:04 -0500

    autocomplete search

---
+++ assets/js/myacsearch.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    $.widget( "custom.catcomplete", $.ui.autocomplete, {
+        _renderMenu: function( ul, items ) {
+            var that = this,
+                currentCategory = "";
+            $.each( items, function( index, item ) {
+                if ( item.category != currentCategory ) {
+                    ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+                    currentCategory = item.category;
+                }
+                that._renderItemData( ul, item ).append(item.thumb);
+            });
+        }
+    });
+	
+	var acs_action = 'myprefix_autocompletesearch';
+	$("#s").catcomplete({
+		source: function(req, response){
+			$.getJSON(MyAcSearch.url+'?callback=?&action='+acs_action, req, response).success(function(data) {
+                if (data.length == 0) {
+                    $('#ui-id-1').html('<h2>No Results</h2>').show();
+                }
+           });
+		},
+		select: function(event, ui) {
+			window.location.href=ui.item.link;
+		},
+        search: function( event, ui ) {
+            // Something here
+        },
+        close: function( event, ui ) {
+            //spomething here
+        },
+		minLength: 3,
+	});
+
+});
