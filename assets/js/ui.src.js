fa1ae3b6e9a16f734f9bfc0b8d27baf66a6c8d7c
Garry Ing <garry.ing@gmail.com>
2013-03-17 15:08:54 -0400

    on load focus on search

--- assets/js/ui.src.js
+++ assets/js/ui.src.js
@@ -1,8 +1,15 @@
 /* ocaduillustration.com */
 
 $(function() {
-	'use strict';
-	$('#year-widget').hover(function(){
-		$('#year-select').slideToggle(120);
-	});
+  'use strict';
+  $('#year-widget').hover(function(){
+    $('#year-select').slideToggle(120);
+  });
+  
+  // on Window Load
+  window.onload = function() {
+    if ($('body').hasClass('home')) {
+      document.getElementById("s").focus();
+    }
+  };
 });
