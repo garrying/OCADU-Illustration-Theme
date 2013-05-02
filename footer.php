86a8cbd776bdf6592e124e19290b3e867553277f
Garry Ing <garry.ing@gmail.com>
2013-05-01 21:02:34 -0400

    tracking 404

--- footer.php
+++ footer.php
@@ -4,6 +4,9 @@
 
 <script>
 	var _gaq=[['_setAccount','UA-16173154-1'],['_trackPageview']];
+	<?php if (is_404()) { ?>
+	_gaq.push(['_trackPageview', '/404.html?page=' + document.location.pathname + document.location.search + '&from=' + document.referrer]);
+	<?php } ?>
 	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 	s.parentNode.insertBefore(g,s)}(document,'script'));
