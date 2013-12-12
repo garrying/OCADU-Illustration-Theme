27f69db3ecde8d382d9721c3306a25d47f9c9d90
Garry Ing <garry.ing@gmail.com>
2013-12-11 20:51:26 -0500

    adding minifying and compass to grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       options: {
-        jshintrc: '.jshintrc'
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
       },
       all: [
         'Gruntfile.js',
         'assets/js/*.js',
-        'assets/js/lib/*.js',
         '!assets/js/app.min.js'
       ]
     },
@@ -23,12 +23,32 @@ module.exports = function(grunt) {
         }
       }
     },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'assets/sass',
+          cssDir: 'assets/stylesheets'
+        }
+      }
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'assets/stylesheets/',
+        src: ['*.css'],
+        dest: 'assets/stylesheets/',
+      }
+    },
     watch: {
       js: {
         files: [
           '<%= jshint.all %>'
         ],
         tasks: ['uglify']
+      },
+      css: {
+        files: 'assets/sass/*.scss',
+        tasks: ['compass', 'cssmin']
       }
     },
     clean: {
@@ -42,12 +62,16 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Register tasks
   grunt.registerTask('default', [
+    'jshint',
     'clean',
     'uglify',
+    'cssmin'
   ]);
   grunt.registerTask('dev', [
     'watch'
