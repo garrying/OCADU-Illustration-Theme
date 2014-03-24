df4479bd507e21e47deeadcb46297c478f79a37b
Garry Ing <garry.ing@gmail.com>
2014-03-23 22:16:47 -0400

    syle edits

--- content-single.php
+++ content-single.php
@@ -49,8 +49,8 @@
 							<li class="nav-previous"><?php next_post_link_plus( array('order_by' => 'post_date', 'format' => '%link', 'in_same_tax' => true, 'link' => '<span>%title</span>') ); ?></li>
 							<li class="nav-next"><?php previous_post_link_plus( array('order_by' => 'post_date', 'format' => '%link', 'in_same_tax' => true, 'link' => '<span>%title</span>') ); ?></li>
 						<?php else : ?>
-							<li class="nav-previous"><?php previous_post_link_plus( array('order_by' => 'post_title', 'format' => '%link', 'in_same_tax' => true, 'link' => '<span class="indicator">◐</span> <span class="truncate">%title</span>') ); ?></li>
-							<li class="nav-next"><?php next_post_link_plus( array('order_by' => 'post_title', 'format' => '%link', 'in_same_tax' => true, 'link' => '<span class="truncate">%title</span> <span class="indicator">◑</span>') ); ?></li>
+							<li class="nav-previous"><?php previous_post_link_plus( array('order_by' => 'post_title', 'format' => '%link', 'in_same_tax' => true, 'link' => '<span class="indicator">○</span> <span class="truncate">%title</span>') ); ?></li>
+							<li class="nav-next"><?php next_post_link_plus( array('order_by' => 'post_title', 'format' => '%link', 'in_same_tax' => true, 'link' => '<span class="truncate">%title</span> <span class="indicator">●</span>') ); ?></li>
 						<?php endif; ?>
 					</ul>
 				</nav><!-- #nav-single -->
