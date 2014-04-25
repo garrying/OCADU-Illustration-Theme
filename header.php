0abd35ab5a123d313d49d83a7e1b9db74db84974
Garry Ing <garry.ing@gmail.com>
2014-04-24 23:39:32 -0400

    edits

--- header.php
+++ header.php
@@ -117,7 +117,7 @@
 <?php if ( is_home() || is_front_page() ) : ?>
 	<div id="intro">
 		<div class="intro-inner">
-			<div class="close"></div>
+			<div class="close" title="Close this block please."></div>
 			<h2>
 				An evolving archive maintained by the Illustration Department at OCAD University. Featuring work from the graduating class of 2014.
 			</h2>
