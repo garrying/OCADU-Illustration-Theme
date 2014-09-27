ed730e1355a5894fca230b9a5d0348b7ad3c1bcf
Garry Ing <garry.ing@gmail.com>
2014-09-27 14:38:35 -0400

    display ads

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-16173154-1', 'ocaduillustration.com');
+  ga('require', 'displayfeatures');
   ga('send', 'pageview');
 </script>
 
