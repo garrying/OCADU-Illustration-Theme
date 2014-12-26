c971e2c061968a112bd82027209b91130a3158ec
Garry Ing <garry.ing@gmail.com>
2014-12-26 11:40:38 -0500

    proper h2 close

--- content.php
+++ content.php
@@ -2,6 +2,6 @@
   <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
     <?php the_post_thumbnail('thumbnail', array('alt' => 'Thumbnail of '.get_the_title().'', 'title' => ''.get_the_title().'' )); ?>
     <h1 class="title"><?php echo get_post_meta($post->ID, 'illu_title', true); ?></h1>
-    <h2 class="illustrator-name"><?php the_title(); ?></h1>
+    <h2 class="illustrator-name"><?php the_title(); ?></h2>
   </a>
 </article><!-- <?php the_title(); ?> -->
