1122b39e9b8a43330687cce335a2be743550ae0c
Garry Ing <garry.ing@gmail.com>
2014-12-30 14:44:09 -0500

    mf2 clean-up

--- content.php
+++ content.php
@@ -1,7 +1,7 @@
 <article <?php post_class('gallery-item'); ?> role="article">
   <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
     <?php the_post_thumbnail('thumbnail', array('alt' => 'Thumbnail of '.get_the_title().'', 'title' => ''.get_the_title().'' )); ?>
-    <h1 class="title"><?php echo get_post_meta($post->ID, 'illu_title', true); ?></h1>
-    <h2 class="illustrator-name"><?php the_title(); ?></h2>
+    <h1 class="title p-name"><?php echo get_post_meta($post->ID, 'illu_title', true); ?></h1>
+    <h2 class="illustrator-name p-author"><?php the_title(); ?></h2>
   </a>
 </article><!-- <?php the_title(); ?> -->
