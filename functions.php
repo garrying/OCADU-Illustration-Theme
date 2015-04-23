355f364a0845221ce7821b85f1a2cdd650064de9
Garry Ing <garry.ing@gmail.com>
2015-04-23 13:28:43 -0400

    social image size

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ if ( ! function_exists( 'ocadu_setup' ) ) :
      * Add image size 
      */
     
-    add_image_size( 'illustrator-social', 600, 315, true );
+    add_image_size( 'illustrator-social', 600 );
 
     /**
      * Let WordPress Manage The Document Title
