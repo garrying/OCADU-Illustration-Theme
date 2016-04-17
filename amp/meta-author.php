a0035da4689c123e8a4d6613720cd87d6f8a8eed
Garry Ing <garry.ing@gmail.com>
2016-04-17 16:10:47 -0400

    style fixes

--- amp/meta-author.php
+++ amp/meta-author.php
@@ -1,6 +1,6 @@
 <li class="amp-wp-byline">
   <?php if ( get_post_meta( $this->get( 'post_id' ), 'illu_email', true ) ) : ?>
-    <a title="Email <?php the_title(); ?>" href="mailto:<?php esc_html_e( get_post_meta( $this->get( 'post_id' ), 'illu_email', true ) ) ?>">
+    <a title="Email <?php the_title(); ?>" href="mailto:<?php echo esc_html( get_post_meta( $this->get( 'post_id' ), 'illu_email', true ) ) ?>">
       <span class="amp-wp-author"><?php echo esc_html( the_title() ); ?></span>
     </a>
   <?php endif; ?>
