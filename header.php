64086de3c512c4683a9e3370275913cf0df91f1d
Garry Ing <garry.ing@gmail.com>
2016-04-23 20:20:11 -0400

    fix brick break points

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width,maximum-scale=1.0,initial-scale=1.0,minimum-scale=1.0,user-scalable=yes" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <?php wp_head(); ?>
 
 </head>
