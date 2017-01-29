e67bb0e74dd7dd5c7ca895f7cca9ba91bd914879
Garry Ing <garry.ing@gmail.com>
2017-01-29 17:24:21 -0500

    ga update

--- footer.php
+++ footer.php
@@ -1,16 +1,12 @@
   </main><!-- #content -->
 
-<?php wp_footer(); ?>
+  <?php wp_footer(); ?>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-16173154-1', 'ocaduillustration.com');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
-</script>
+  <script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create','UA-16173154-1','auto');ga('send','pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 
-</body>
+  </body>
 </html>
