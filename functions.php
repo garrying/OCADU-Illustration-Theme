971207439210f44acbb165068e85b455542741d2
Garry Ing <garry.ing@gmail.com>
2017-01-29 22:46:26 -0500

    style fix

--- functions.php
+++ functions.php
@@ -378,7 +378,7 @@ function ocadu_modify_attachment_link( $markup, $id, $size, $permalink ) {
   $image_url = wp_get_attachment_image_src( $id, 'large' )[0];
   $image_srcset = wp_get_attachment_image_srcset( $id );
   $image_sizes = wp_get_attachment_image_sizes( $id, 'large' );
-  $image_caption = get_post($id)->post_excerpt;
+  $image_caption = get_post( $id )->post_excerpt;
   if ( ! $permalink ) {
     $markup = str_replace( '<a href', '<a class="gallery-icon-anchor" data-srcset="' . $image_srcset . '" data-src-large="' . $image_url . '" data-caption="' . $image_caption . '" data-sizes="' . $image_sizes . '" href', $markup );
   }
