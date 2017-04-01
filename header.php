48e31205c11682a7dd31ffcc8e3b142e3d726b67
Garry Ing <garry.ing@gmail.com>
2017-04-01 15:54:16 -0400

    09 to 17

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
           </div>
           <div class="header-items-wrapper">
             <div class="header-item-link">
-              <button id="year-select-link" aria-controls="panel-year-select" data-panel="year-select" class="header-item" title="Navigate years">Years</button>
+              <button id="year-select-link" aria-controls="panel-year-select" data-panel="year-select" class="header-item" title="Navigate years">09–17</button>
             </div>
             <div class="header-item-link">
               <button id="search-link" aria-controls="panel-search" data-panel="search-container" class="header-item" title="Search archives">Search</button>
