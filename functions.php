71a41e6d8700cbf3bc9bcf8a2eb40807ac831840
Garry Ing <garry.ing@gmail.com>
2017-04-26 09:43:46 -0400

    year change

--- functions.php
+++ functions.php
@@ -279,7 +279,7 @@ function ocadu_social_meta() {
 
   }
   if ( is_home() || is_archive() ) {
-    $social_description = 'Presented by the Illustration Department at OCAD U featuring work from the graduating class of 2016.';
+    $social_description = 'Presented by the Illustration Department at OCAD U featuring work from the graduating class of 2017.';
     if ( is_home() ) {
       $social_title = get_bloginfo( 'name' );
     } else {
