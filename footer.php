30f5e40279928af24d38935e68fed04b0e626713
Garry Ing <garry.ing@gmail.com>
2018-02-22 21:42:35 -0500

    revert

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
     window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
     ga('create','UA-16173154-1','auto');ga('send','pageview')
   </script>
+  <?php  // phpcs:ignore  ?>
   <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 
   </body>
