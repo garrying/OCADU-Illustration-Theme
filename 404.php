fd16c0e53096c3ee6b56583f572533c9d16dec8d
Garry Ing <garry.ing@gmail.com>
2018-04-14 16:51:29 -0400

    404 edit

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
 
     <header class="entry-header">
       <h1 class="entry-title"><?php esc_html_e( 'Page Not Found.', 'ocaduillustration' ); ?></h1>
-      <p>The page you requested could not be found. <br />It might have been moved or renamed.</p>
+      <p class="entry-body">The page you requested could not be found<br />It might have been moved or renamed.</p>
     </header>
   </div>
 
