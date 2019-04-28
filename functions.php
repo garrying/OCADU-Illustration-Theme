a9d4edc7f63f3d60ba7f522b4258a3a09ca91da4
Garry Ing <garry.ing@gmail.com>
2019-04-28 18:16:58 -0400

    string up

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ if ( ! function_exists( 'ocaduillustration_scripts' ) ) {
   function ocaduillustration_scripts() {
     if ( ! is_admin() ) {
       wp_deregister_script( 'wp-embed' );
-      wp_register_script( 'app', get_template_directory_uri() . '/assets/dist/app.js?7926118495', '', '', true );
+      wp_register_script( 'app', get_template_directory_uri() . '/assets/dist/app.js?7926118494', '', '', true );
       wp_enqueue_script( 'app' );
     }
   }
@@ -104,7 +104,7 @@ function ocaduillustration_fonts() {
 }
 
 function ocaduillustration_styles() {
-  wp_register_style( 'ocadustyles', get_template_directory_uri() . '/assets/dist/main.css?7926118495' );
+  wp_register_style( 'ocadustyles', get_template_directory_uri() . '/assets/dist/main.css?7926118494' );
   wp_enqueue_style( 'ocadustyles' );
 }
 
