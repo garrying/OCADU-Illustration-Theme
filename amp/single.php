ec33b81ad5dfbb87459f18ef0c2f21a6aff3972b
Garry Ing <garry.ing@gmail.com>
2020-02-09 17:52:07 -0500

    comment update

--- amp/single.php
+++ amp/single.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html amp <?php echo AMP_HTML_Utils::build_attributes_string( $this->get( 'html_tag_attributes' ) ); /* WPCS: xss ok. */ ?>>
+<html amp <?php echo AMP_HTML_Utils::build_attributes_string( $this->get( 'html_tag_attributes' ) ); // phpcs:ignore ?>>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
