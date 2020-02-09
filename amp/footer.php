a4f618a0c2cf280deb55a25226fd071b3311089f
Garry Ing <garry.ing@gmail.com>
2020-02-09 17:40:33 -0500

    ignore line

--- amp/footer.php
+++ amp/footer.php
@@ -5,7 +5,7 @@
     $ocaduillustration_description = get_bloginfo( 'description', 'display' );
       if ( $ocaduillustration_description_description || is_customize_preview() ) :
       ?>
-        <p class="site-description"><?php echo $ocaduillustration_description_description; /* WPCS: xss ok. */ ?></p>
+        <p class="site-description"><?php echo $ocaduillustration_description_description; // phpcs:ignore ?></p>
     <?php endif; ?>
     <a href="#top" class="back-to-top">Back to top</a>
   </div>
