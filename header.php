fde4e501694dabd6907e30ce464da7b6eb704bdf
Garry Ing <garry.ing@gmail.com>
2020-05-10 15:23:24 -0400

    fix

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="profile" href="https://gmpg.org/xfn/11" />
-  <link rel="icon" href="">
   <?php wp_head(); ?>
 
 </head>
