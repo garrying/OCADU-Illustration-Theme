f83c5a1bd06eb8b651b5b936bd3c56b97a405311
Garry Ing <ging@vmware.com>
2020-08-12 11:03:36 -0400

    remove ga

--- footer.php
+++ footer.php
@@ -2,12 +2,5 @@
 
   <?php wp_footer(); ?>
 
-  <script>
-    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
-    ga('create','UA-16173154-1','auto');ga('send','pageview')
-  </script>
-  <?php  // phpcs:ignore  ?>
-  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
-
   </body>
 </html>
