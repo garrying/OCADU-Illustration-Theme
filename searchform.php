f0e41ba5e7c3253ac21d3b45d09633bd61f9124d
Garry Ing <ging@vmware.com>
2020-12-24 18:44:21 -0500

    init swap

--- searchform.php
+++ searchform.php
@@ -2,7 +2,6 @@
   <label>
     <span class="screen-reader-shortcut"><?php echo esc_html( 'Search for:' ); ?></span>
     <input type="search" class="search-field pill" id="autocomplete" placeholder="<?php echo esc_attr_x( 'Search illustrators', 'placeholder', 'ocaduillustration' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'ocaduillustration' ); ?>" />
-    <span class="search-loader"></span>
   </label>
   <button type="submit" class="search-submit pill"><?php echo esc_html( 'Search' ); ?></button>
 </form>
