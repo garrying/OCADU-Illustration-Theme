9a72246015c7037246646890911b337fcbace2cc
Garry Ing <garry.ing@gmail.com>
2021-05-04 20:55:09 -0400

    bust

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ if ( ! function_exists( 'ocaduillustration_scripts' ) ) {
   function ocaduillustration_scripts() {
     if ( ! is_admin() ) {
       wp_deregister_script( 'wp-embed' );
-      wp_register_script( 'app', get_template_directory_uri() . '/assets/dist/app.js?1600001469', '', '2021', true );
+      wp_register_script( 'app', get_template_directory_uri() . '/assets/dist/app.js?1620176090', '', '2021', true );
       wp_enqueue_script( 'app' );
     }
   }
@@ -104,7 +104,7 @@ function ocaduillustration_fonts() {
 }
 
 function ocaduillustration_styles() {
-  wp_register_style( 'ocadustyles', get_template_directory_uri() . '/assets/dist/main.css?1600001469', '', '2021' );
+  wp_register_style( 'ocadustyles', get_template_directory_uri() . '/assets/dist/main.css?1620176090', '', '2021' );
   wp_enqueue_style( 'ocadustyles' );
 }
 
@@ -248,11 +248,11 @@ function ocaduillustration_get_socialimage( $image_type = 'fb' ) {
   }
 
   if ( 'twitter-index' === $image_type ) {
-    $socialimg = get_template_directory_uri() . '/thumb-twitter.jpg?1600001469';
+    $socialimg = get_template_directory_uri() . '/thumb-twitter.jpg?1620176090';
   }
 
   if ( empty( $socialimg ) ) {
-    $socialimg = get_template_directory_uri() . '/thumb.jpg?1600001469';
+    $socialimg = get_template_directory_uri() . '/thumb.jpg?1620176090';
   }
 
   return $socialimg;
