97b921598441b25c5a8e4d911b18cf9595366e48
Garry Ing <garry.ing@gmail.com>
2022-05-09 14:55:51 -0400

    header up

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#ff5722" />
   <?php wp_head(); ?>
 </head>
 
