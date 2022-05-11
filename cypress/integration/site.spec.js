5c7d1922a7806ed267a652489ace09b899f3d6f9
Garry Ing <garry.ing@gmail.com>
2022-05-10 21:07:30 -0400

    spec

--- cypress/integration/site.spec.js
+++ cypress/integration/site.spec.js
@@ -4,7 +4,7 @@ describe('ocaduillustration.com', () => {
   })
 
   it('displays all illustrators', () => {
-    cy.get('.gallery-item').should('have.length', 83)
+    cy.get('.gallery-item').should('have.length', 48)
   })
 
   it('can open year selection panel', () => {
@@ -13,14 +13,15 @@ describe('ocaduillustration.com', () => {
       cy.root()
         .should('be.visible')
         .find('.year-list-item')
-        .should('have.length', 15)
+        .should('have.length', 16)
         .first()
         .find('.year-item')
         .first()
-        .should('have.text', '2021')
+        .should('have.text', '2022')
         .root()
         .find('.close-panel')
         .click()
+        .wait(1000)
         .root()
         .should('be.hidden')
     })
@@ -48,6 +49,7 @@ describe('ocaduillustration.com', () => {
         .root()
         .find('.close-panel')
         .click()
+        .wait(1000)
         .root()
         .should('be.hidden')
     })
@@ -84,6 +86,81 @@ describe('ocaduillustration.com', () => {
         .find('.year-item')
         .first()
         .click()
+    })
+  })
+})
+
+describe('ocaduillustration.com/illustrators/*', () => {
+  beforeEach(() => {
+    cy.visit('https://www.ocaduillustration.com/illustrators/yiren-tang/')
+  })
+
+  const openModal = () => {
+    cy.get('.gallery-item').first().within(() => {
+      cy.root()
+        .find('a')
+        .click()
+    })
+  }
+
+  it('displays all information', () => {
+    cy.get('.illustrator-meta-wrapper-inner').should('be.visible')
+    cy.get('.thesis-title').contains('A slice of life')
+    cy.get('.illustrator-meta-name').contains('Yiren Tang')
+    cy.get('.thesis-description p').invoke('text').should('have.length', 221)
+    cy.get('.illustrator-meta-items a').should('have.length', 2)
+  })
+
+  it('displays navigation', () => {
+    cy.get('.illustrator-nav-single-wrapper').should('be.visible')
+    cy.get('.section-indicator-single')
+      .click()
+      .location('pathname').should('eq', '/year/2022/')
+      .go('back')
+    cy.get('.nav-previous a')
+      .click()
+      .location('pathname').should('eq', '/illustrators/xin/')
+      .go('back')
+    cy.get('.nav-next a')
+      .click()
+      .location('pathname').should('eq', '/illustrators/yoana-vasileva/')
+  })
+
+  it('displays all work', () => {
+    cy.get('.gallery-item').should('have.length', 7)
+    cy.get('#image-modal').should('be.hidden')
+  })
+
+  it('can clickthrough to an image', () => {
+    openModal()
+    cy.get('#image-modal').should('be.visible')
+  })
+
+  it('can close image modal', () => {
+    openModal()
+    cy.get('#image-modal').within(() => {
+      cy.root()
+        .find('.close-panel')
+        .click()
+        .wait(1000)
+        .root()
+        .should('be.hidden')
+    })
+  })
+
+  it('can navigate image modal', () => {
+    openModal()
+    cy.get('#image-modal').within(() => {
+      cy.root()
+        .find('#full-image')
+        .invoke('attr', 'src')
+        .should('eq', 'https://www.ocaduillustration.com/files/yiren_tang_01.jpg')
+        .root()
+        .find('#full-image')
+        .click()
+        .wait(1000)
+        .invoke('attr', 'src')
+        .should('eq', 'https://www.ocaduillustration.com/files/yiren_tang_02.jpg')
     })
   })
 })
