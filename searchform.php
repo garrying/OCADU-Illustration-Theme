ed5f43c3440881cbfad58f66ff4488a9cd1b5525
Garry Ing <garry.ing@gmail.com>
2022-05-17 21:28:47 -0400

    fix search form markup

--- searchform.php
+++ searchform.php
@@ -1,7 +1,5 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <label>
-    <span class="screen-reader-shortcut"><?php echo esc_html( 'Search for:' ); ?></span>
-    <input type="search" class="search-field input-reset" id="autocomplete" placeholder="<?php echo esc_attr_x( 'Search illustrators', 'placeholder', 'ocaduillustration' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'ocaduillustration' ); ?>" />
-  </label>
+  <label for="autocomplete" class="screen-reader-shortcut"><?php echo esc_html( 'Search' ); ?></label>
+  <input type="search" class="search-field input-reset" id="autocomplete" placeholder="<?php echo esc_attr_x( 'Search illustrators', 'placeholder', 'ocaduillustration' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'ocaduillustration' ); ?>" />
   <button type="submit" class="search-submit"><?php echo esc_html( 'Search' ); ?></button>
 </form>
