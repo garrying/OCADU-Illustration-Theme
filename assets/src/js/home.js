6115f458e4400df8fac7e270e4e1a2aa6bc541d4
Garry Ing <garry.ing@gmail.com>
2023-03-25 17:00:19 -0400

    remove react

--- assets/src/js/home.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-
-function App () {
-  return <></>
-}
-
-const root = createRoot(document.querySelector('#root'))
-root.render(<App />)
