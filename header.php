7a16b8cf6d4d4ca48241038c55cec590bdcdc0b2
Garry Ing <garry.ing@gmail.com>
2023-04-27 10:56:33 -0400

    updates

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="theme-color" content="#ff5722" />
+  <meta name="theme-color" content="#444444" />
   <?php wp_head(); ?>
 </head>
 
