7d8ce2164e82e5c60b66c6ea325c2110cd2c407e
Garry Ing <garry.ing@gmail.com>
2023-04-29 12:44:10 -0400

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env']
         }
       }, {
         test: /\.svg$/,
