ff08d18a12bc44f12300ab85f1e41df3b346501c
Garry Ing <garry.ing@gmail.com>
2024-04-28 19:10:30 -0400

    fixes

--- header.php
+++ header.php
@@ -15,7 +15,6 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="theme-color" content="#444444" />
   <?php wp_head(); ?>
 </head>
 
