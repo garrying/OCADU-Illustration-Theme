55476a7069f1d60eb5c0950d36ca128468a2f842
Garry Ing <garry.ing@gmail.com>
2025-04-24 22:25:07 -0400

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,68 +3,72 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.exports = {
-  entry: {
-    app: './assets/src/js/app.js',
-    home: './assets/src/js/home.js'
-  },
-  output: {
-    path: path.join(__dirname, './assets/dist/'),
-    filename: '[name].js',
-    assetModuleFilename: './fonts/[name][ext]'
-  },
-  mode: 'development',
-  context: __dirname,
-  watchOptions: {
-    ignored: '**/node_modules'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: './'
-            }
-          },
-          'css-loader',
-          'sass-loader',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode !== 'production'
+  return {
+    entry: {
+      app: './assets/src/js/app.js',
+      home: './assets/src/js/home.js'
+    },
+    output: {
+      path: path.join(__dirname, './assets/dist/'),
+      filename: '[name].js',
+      assetModuleFilename: './fonts/[name][ext]'
+    },
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'source-map' : false,
+    context: __dirname,
+    watchOptions: {
+      ignored: '**/node_modules'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: './'
+              }
+            },
+            'css-loader',
+            'sass-loader',
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        {
+          test: /\.svg$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            name: './images/[name].[ext]'
+          }
+        },
+        {
+          test: /\.(svg|eot|ttf|woff|woff2)$/i,
+          type: 'asset'
         }
-      },
-      {
-        test: /\.svg$/,
-        exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          name: './images/[name].[ext]'
-        }
-      },
-      {
-        test: /\.(svg|eot|ttf|woff|woff2)$/i,
-        type: 'asset'
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './assets/src/images', to: './images' }]
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main.css'
-    })
-  ],
-  stats: 'normal'
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: './assets/src/images', to: './images' }]
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'main.css'
+      })
+    ],
+    stats: 'normal'
+  }
 }
